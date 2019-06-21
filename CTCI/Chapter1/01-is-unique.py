cd1f96761162183118305d9972ce11b4c45023ab
Nihal <nihal.cse223@gmail.com>
2019-06-21 12:21:27 -0600

    is unique

--- CTCI/Chapter1/01-is-unique.py
+++ CTCI/Chapter1/01-is-unique.py
@@ -1,1 +1,33 @@
+# with additional data structure
+def is_unique_ds(s):
+    # ASCII charset is 128
+    if len(string) > 128:
+        return False
 
+    letters = {}
+    for char in s:
+        if char in letters:
+            return False
+        letters[char] = True
+
+    return True
+
+# without additional data structure
+def is_unique_no_ds(s):
+    # ASCII charset is 128
+    if len(string) > 128:
+        return False
+
+    sorted_s = sorted(s)
+    for i, char in enumerate(sorted_s[:-1]):
+        if sorted_s[i] == sorted_s[i+1]:
+            return False
+
+    return True
+
+
+if __name__  == '__main__':
+    string = "welgwengiwgnwipenbwieb"
+    res1 = is_unique_ds(string)
+    res2 = is_unique_no_ds(string)
+    print(res1,res2)
