c3051acb95c1bbaa08aee7538d98112a4569e121
Nihal <nihal.cse223@gmail.com>
2019-06-22 18:13:04 -0600

    dups followup

--- CTCI/Chapter2/01-remove-dups.py
+++ CTCI/Chapter2/01-remove-dups.py
@@ -18,11 +18,23 @@ def remove_dups(head):
                 values[node.next.data] = True
                 node = node.next
 
+def remove_dups_followup(head):
+    node = head
+    if node:
+        while node:
+            runner = node
+            while runner.next:
+                if runner.next.data == node.data:
+                    runner.next = runner.next.next
+                else:
+                    runner = runner.next
+            node = node.next
+
 
 class Test(unittest.TestCase):
     def test_remove_dups(self):
         head = Node(1, Node(3, Node(3, Node(1, Node(5, None)))))
-        remove_dups(head)
+        remove_dups_followup(head)
         self.assertEqual(head.data, 1)
         self.assertEqual(head.next.data, 3)
         self.assertEqual(head.next.next.data, 5)
