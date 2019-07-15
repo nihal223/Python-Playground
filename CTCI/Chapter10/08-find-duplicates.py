601bf1ad367e35bb8780aaf849c2cae23cf23753
Nihal <nihal.cse223@gmail.com>
2019-07-14 20:09:16 -0600

    no idea

---
+++ CTCI/Chapter10/08-find-duplicates.py
@@ -0,0 +1,24 @@
+import unittest
+
+
+def find_duplicates(array):
+  bv = [0] * 512
+  duplicates = []
+  for n in array:
+    bit = 1 << (n % 64)
+    if bv[n // 64] & bit:
+      duplicates.append(n)
+    else:
+      bv[n // 64] |= bit
+  return duplicates
+
+
+class Test(unittest.TestCase):
+  def test_find_duplicates(self):
+    array = [1, 2, 3, 4, 55, 20000, 20001, 20002, 20003, 17, 18, 19, 20, 22, 23,
+             7, 2, 3, 3, 55, 20002, 20004, 20005, 20006, 16, 18, 22, 24, 25, 26]
+    self.assertEqual(find_duplicates(array), [2, 3, 3, 55, 20002, 18, 22])
+
+
+if __name__ == "__main__":
+  unittest.main()
